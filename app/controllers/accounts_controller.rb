50e64cf1f86a23e752a13084112ed0c5678ffcfb
Jenny Kam <jkam17@gmail.com>
2019-09-28 23:55:22 -0400

    create files for new and show account views, build basic controller actions for new account creation with new account form

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,26 @@
 class AccountsController < ApplicationController
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.create(account_params)
+    if @account.valid?
+      redirect_to account_path(@account)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :email, :password)
+  end
+
 end
