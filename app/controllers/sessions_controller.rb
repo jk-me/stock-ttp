ba9fe9888f39c40516786b74d3702bdcca6922ab
Jenny Kam <jkam17@gmail.com>
2019-09-29 21:29:10 -0400

    rename sessions controller (+views/directories), complete login feature functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @account = Account.find_by(name: sess_params[:name])
-    byebug
+    @account = Account.find_by(email: sess_params[:email])
     if @account
       if @account.authenticate(sess_params[:password])
         session[:account_id] = @account.id
         redirect_to account_path(@account)
       else
-        flash[:error] = 'Wrong accountname or password.'
+        # flash[:error] = 'Wrong accountname or password.'
         redirect_to new_session_path
       end
     else
-      flash[:error] = 'Wrong accountname or password.'
+      # flash[:error] = 'Wrong accountname or password.'
       redirect_to new_session_path
     end
   end
@@ -28,7 +27,7 @@ class SessionsController < ApplicationController
   private
 
   def sess_params
-    params.require(:account).permit(:name, :password)
+    params.require(:account).permit(:email, :password)
   end
 
 end
